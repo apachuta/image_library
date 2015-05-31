e12b25cfecc470c00dd340bbb5c8b68839d85d99
Jakub Ocwieja <netkuba@students.mimuw.edu.pl>
2015-05-31 20:09:52 +0200

    2d point interface added

---
+++ image_library/src/main/java/three_d/math/Point2d.java
@@ -0,0 +1,15 @@
+package three_d.math;
+
+public class Point2d {
+	
+	Point2d() {
+		this(0, 0);
+	}
+	
+	Point2d(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	private double x, y;
+}
